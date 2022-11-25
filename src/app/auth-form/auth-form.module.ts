9a788437ecae13f517112cf9122af80f0517e60a
chougrani <chougranisouhail@gmail.com>
2022-11-25 18:45:15 +0100

    init branch

--- src/app/auth-form/auth-form.module.ts
+++ src/app/auth-form/auth-form.module.ts
@@ -5,11 +5,12 @@ import { AuthFormComponent } from './auth-form.component';
 import { AngularMaterialModule } from '../angular-material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRemembreMeComponent } from './auth-remembre-me.component';
+import { AuthMessageComponent } from './auth-message.component';
 
 @NgModule({
     imports: [CommonModule,ReactiveFormsModule,AngularMaterialModule],
-    exports: [AuthFormComponent,AuthRemembreMeComponent],
-    declarations: [AuthFormComponent, AuthRemembreMeComponent],
+    exports: [AuthFormComponent,AuthRemembreMeComponent,AuthMessageComponent],
+    declarations: [AuthFormComponent, AuthRemembreMeComponent,AuthMessageComponent],
     providers: [],
 })
 export class AuthFormModule { }

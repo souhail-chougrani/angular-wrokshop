d949724d6ce6283afb188e90e6b478c2d63c9e97
chougrani <chougranisouhail@gmail.com>
2022-12-02 14:48:05 +0100

    remove authcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AuthFormComponent } from './auth-form/auth-form.component';
 import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthFormComponent
   ],
   imports: [
     BrowserModule,

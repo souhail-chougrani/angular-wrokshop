f283629094e6ff4ef829ba05c0fbc7bc7906b597
chougrani <chougranisouhail@gmail.com>
2022-12-11 17:49:49 +0100

    remove auth-form import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { AfterContentInit, AfterViewInit, Component, ElementRef, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
-import { AuthFormComponent } from './auth-form/auth-form.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',

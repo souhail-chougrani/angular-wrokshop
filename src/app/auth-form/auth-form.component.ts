7b9f5f3cfdeaafc159b4b44676dc70e1ce0bd416
Souhail Chougrani <048259@INTELCIA.COM>
2022-12-18 20:43:34 +0100

    add title

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -10,6 +10,7 @@ export class AuthFormComponent implements OnInit {
   @Output()
   submited:EventEmitter<any> = new EventEmitter<any>();
   logingForm!:FormGroup;
+  title = 'Login'
   constructor(private fb:FormBuilder) {
     this.logingForm = this.fb.group({
       username:null,

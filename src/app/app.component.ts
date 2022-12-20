a50520238050785694747e05a7352ff9a9f47a0b
chougrani <chougranisouhail@gmail.com>
2022-12-20 12:13:15 +0100

    full example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+import { tap } from 'rxjs';
+import { AuthFormComponent } from './auth-form/auth-form.component';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('entry',{read:ViewContainerRef}) entry !: ViewContainerRef
+  @ViewChild('tmp') tmp !:TemplateRef<any>
   title = 'appName';
-
+  ctx = {name:'Mbappé',$implicit:"French"}
+  constructor(){
+  }
   login($event:any){
     console.log('loging',$event)
+  }
+
+  ngAfterViewInit(){
+
+    // create component dynamicly
+  //  const component = this.entry.createComponent(AuthFormComponent);
+  //   component.instance.title = 'Create Account'
+  //   component.instance.submited.pipe(tap(this.login)).subscribe();
+    // this.entry.createComponent(AuthFormComponent);
+
+    // create template dynamicly
+    // this.entry.createEmbeddedView(this.tmp,{$implicit:'France', name:"Mbappé"})
   }
 }

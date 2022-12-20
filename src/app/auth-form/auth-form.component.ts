b051467d38218f873e25cfb69cd7948bd2436553
chougrani <chougranisouhail@gmail.com>
2022-12-20 10:58:19 +0100

    full example

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -1,12 +1,17 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ContentChild, EventEmitter, OnInit, Output ,AfterContentInit, ContentChildren, QueryList } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { filter, tap } from 'rxjs';
+import { AuthRemembreMeComponent } from './auth-remembre-me.component';
 
 @Component({
   selector: 'app-auth-form',
   templateUrl: './auth-form.component.html',
   styleUrls: ['./auth-form.component.scss']
 })
-export class AuthFormComponent implements OnInit {
+export class AuthFormComponent implements OnInit,AfterContentInit {
+  @ContentChild(AuthRemembreMeComponent) remembre !: AuthRemembreMeComponent  
+  @ContentChildren(AuthRemembreMeComponent) remembres !: QueryList<AuthRemembreMeComponent>  
   @Output()
   submited:EventEmitter<any> = new EventEmitter<any>();
   logingForm!:FormGroup;
@@ -16,6 +21,16 @@ export class AuthFormComponent implements OnInit {
       username:null,
       password:null
     })
+  }
+  ngAfterContentInit(): void {
+    
+    this.remembre.remember.subscribe(($event:MatSlideToggleChange)=>this.showMessage =$event.checked)
+    if(this.remembres){
+      console.log(this.remembres)
+      this.remembres.forEach(co=>co.remember.pipe(
+        tap(($event:MatSlideToggleChange)=>this.showMessage =$event.checked)
+      ).subscribe())
+    }
   }
 
   ngOnInit(): void {
